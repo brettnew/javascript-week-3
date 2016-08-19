bfe9227afb0414a3e4a9c97353bda1113be1f301
Brett New <brett.new91@gmail.com>
2016-08-19 09:23:16 -0700

    establish one-to-many relationship between question and answers.

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr(),
   author: DS.attr(),
-  notes: DS.attr()
+  notes: DS.attr(),
+  answer: DS.hasMany('answer', {async: true})
 });
